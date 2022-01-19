8f8cd9945a0423f2babac53c1676db503e3cde10
crazygeeks1 <crazygeeks22@gmail.com>
2022-01-19 18:10:37 +0530

    Nativescript Cashflow Project
    Crazy Geeks developed this nativescript project.

---
+++ cashflow/src/app/home/home.component.ts
@@ -0,0 +1,65 @@
+import { ItemEventData } from "tns-core-modules/ui/list-view"
+import { Component, OnInit } from "@angular/core";
+import * as camera from "@nativescript/camera";
+import { Image } from "tns-core-modules/ui/image";
+
+@Component({
+    selector: "Home",
+    moduleId: module.id,
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.css"]
+})
+export class HomeComponent implements OnInit {
+
+    listPickerCountries: Array<string> = ["Australia", "Belgium", "Bulgaria", "Canada", "Switzerland",
+        "China", "Czech Republic", "Germany", "Spain", "Ethiopia", "Croatia", "Hungary",
+        "Italy", "Jamaica", "Romania", "Russia", "United States"];
+    selectedListPickerIndex: number = 0;
+
+    htmlString: string = "<div><h1>HtmlView demo</h1><h2>in NativeScript App</h2></div>";
+
+    Category: { Item: string, Amount: string, Total: string}[] = [
+        { Item: "Category", Amount: "Amount", Total: "Total" },
+        { Item: "Lunch", Amount: "500", Total: "" },
+        { Item: "Transport", Amount: "150", Total: "" },
+        { Item: "Shirt", Amount: "600", Total: "" },
+        { Item: "Electronics", Amount: "3000", Total: "" },
+        { Item: "Coffee", Amount: "100", Total: "" },
+        { Item: "Shoes", Amount: "500", Total: "" },
+        { Item: "Petrol", Amount: "500", Total: "" },
+        { Item: "Noodles", Amount: "150", Total: "" },
+        { Item: "Grocery", Amount: "2000", Total: "15000" },
+    ];
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+    
+    onItemTap(args: ItemEventData): void {
+        console.log('Item with index: ' + args.index + ' tapped');
+    }
+
+    navigateAdd(){
+
+        if(camera.isAvailable) {
+        camera.requestPermissions().then(
+            function success() {
+                camera.takePicture().
+                then((imageAsset) => {
+                    console.log("Result is an image asset instance");
+                    var image = new Image();
+                    image.src = imageAsset;
+                }).catch((err) => {
+                    console.log("Error -> " + err.message);
+                });
+            }, 
+            function failure() {
+
+        });
+        } else {
+            console.log("Camera Not Available!!");
+        }
+    }
+}
